759e9b088497b342c456f609ad3762895f25de32
atta04 <136908865+atta04@users.noreply.github.com>
2023-11-29 13:24:42 +0700

    Add files via upload

---
+++ Pertemuan3.sql
@@ -0,0 +1,15 @@
+SELECT * FROM mahasiswa m 
+
+SELECT nim FROM mahasiswa m 
+
+SELECT m.nama FROM mahasiswa m 
+
+SELECT m.nama, m.nim FROM mahasiswa m 
+
+SELECT mahasiswa.nama, mahasiswa.nim, mahasiswa.asal FROM mahasiswa
+
+SELECT nama AS name FROM mahasiswa m 
+
+SELECT nama AS name, nim, asal FROM mahasiswa
+
+SELECT nama AS NAMA, nim AS NIM, asal AS ASAL FROM mahasiswa 
